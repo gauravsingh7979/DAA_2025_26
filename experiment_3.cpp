4122922229b9038028d07c5bb22800d7f83426cc
gauravsingh7979 <24bet10277@cuchd.in>
2026-01-29 01:02:20 +0530

    Add files via upload

---
+++ experiment_3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    char attendance[N];
+    for (int i = 0; i < N; i++) {
+        cin >> attendance[i];
+    }
+
+    unordered_map<int, int> mp;
+
+    int prefixSum = 0;
+    int maxLength = 0;
+
+    mp[0] = -1;
+
+    for (int i = 0; i < N; i++) {
+
+        if (attendance[i] == 'P')
+            prefixSum += 1;
+        else
+            prefixSum -= 1;
+
+        if (mp.find(prefixSum) != mp.end()) {
+
+            int length = i - mp[prefixSum];
+            if (length > maxLength)
+                maxLength = length;
+        }
+        else {
+            
+            mp[prefixSum] = i;
+        }
+    }
+
+    cout << maxLength << endl;
+
+    return 0;
+}
